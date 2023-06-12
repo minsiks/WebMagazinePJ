7a56121f795e678075882658527b269c03b95722
minsiks <k_ms222@naver.com>
2023-06-12 14:07:24 +0900

    Mabatis type-aliases 패키지 설정 변경

--- src/main/java/com/aza/config/DatabaseConfig.java
+++ src/main/java/com/aza/config/DatabaseConfig.java
@@ -1,21 +1,16 @@
 package com.aza.config;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.boot.autoconfigure.SpringBootVFS;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
@@ -23,11 +18,13 @@ public class DatabaseConfig {
     @Value("${spring.datasource.mapper-locations}")
     private String mpperLocations;
 
+	@Value("${mybatis.type-aliases-package}")
+	private String typeAliasesPackage;
     @Bean
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
         sqlSessionFactoryBean.setDataSource(dataSource);
-//        sqlSessionFactoryBean.setTypeAliasesPackage(typeAliasesPackage);
+        sqlSessionFactoryBean.setTypeAliasesPackage(typeAliasesPackage);
         sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(mpperLocations));
         sqlSessionFactoryBean.setVfs(SpringBootVFS.class);
 
