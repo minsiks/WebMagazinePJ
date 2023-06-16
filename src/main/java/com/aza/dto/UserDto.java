2e4b8432419e2f8da9ddd147b8c006f7ad9ef8ef
shinbeomsik <sbsdkssud@gmail.com>
2023-06-16 16:00:08 +0900

    패스워드 유효성

--- src/main/java/com/aza/dto/UserDto.java
+++ src/main/java/com/aza/dto/UserDto.java
@@ -3,6 +3,7 @@ package com.aza.dto;
 import java.util.Date;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import com.aza.domain.User;
@@ -23,15 +24,15 @@ public class UserDto {
 	@NotBlank(message = "아이디는 필수 입력값입니다.")
 	private String userId;
 	@NotBlank(message = "비밀헌호는 필수 입력값입니다.")
-	@Pattern(regexp = "/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/",
-     message = "영문 숫자 특수기호 조합 8자리 이상입니다.")
+	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$", 
+	message = "영문 숫자 특수기호 조합 8자리 이상입니다.")
 	private String userPwd;
 	@NotBlank(message = "이름은 필수 입력값입니다.")
 	private String userName;
 	private String userGender;
 	@NotBlank(message = "전화번호는 필수 입력값입니다.")
 	private String userPhoneNo;
-	@NotBlank(message = "생년월일은 필수 입력값입니다.")
+	@NotNull(message = "생년월일은 필수 입력값입니다.")
 	private Date userBirth;
 	@NotBlank(message = "이메일은 필수 입력값입니다.")
 	private String userEmail;
